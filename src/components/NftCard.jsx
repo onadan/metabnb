d5164a3be1330cf906d30460f428b7d7eb9152c4
onadan <onadan45@gmail.com>
2022-11-17 04:53:41 +0100

    nft display card fixed

--- src/components/NftCard.jsx
+++ src/components/NftCard.jsx
@@ -4,17 +4,17 @@ import { HiHeart } from "react-icons/hi";
 const NftCard = ({ image }) => {
   return (
     <div className="w-[280px] h-[380px] border rounded-lg p-4 overflow-hidden relative">
-      <div className="w-full h-auto">
+      <div className="w-full max-h-[16.5rem] h-full rounded-2xl overflow-hidden shadow-lg">
         {image ? (
-          <img src={image} alt="nft_image" className="object-cover" />
+          <img src={image} alt="nft_image" className="object-cover h-full w-auto" />
         ) : (
-          <div className="flex justify-center items-center h-full font-bold text-md">Preview not available</div>
+          <div className="flex justify-center items-center w-full h-full text-sm bg-gray-300 border">Preview not available</div>
         )}
         <div className="absolute top-4 right-5 text-xl p-2 text-gray-300 ">
           <HiHeart />
         </div>
       </div>
-      <div className="pt-2 pb-1 flex justify-between items-center">
+      <div className="pt-3 pb-1 flex justify-between items-center">
         <p className="text-xs">Desert King</p>
         <p className="font-bold text-xs">1MBT per night</p>
       </div>
